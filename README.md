7c907fb8c20ef98291525a215eefa91e961b9dec
CGW1996 <s22760081@gmail.com>
2020-07-15 14:38:17 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nutfit
+ nutfit
---
+++ js/cover.js
@@ -0,0 +1,30 @@
+$("#nutCover").mouseenter(function () {
+    // $("#nutCoverTitle").css("color", "black");
+    $(".nutImg").css({ "opacity": "0.8", "transition": ".8s" })
+    $("#nutLine").css({"width":"150px", "transition": ".8s"})
+})
+$("#nutCover").mouseleave(function () {
+    // $("#nutCoverTitle").css("color", "white")
+    $(".nutImg").css("opacity", ".5")
+    $("#nutLine").css({"width":"300px", "transition": ".8s"})
+})
+$("#nttCover").mouseenter(function () {
+    // $("#nttCoverTitle").css("color", "black");
+    $(".nttImg").css({ "opacity": "0.8", "transition": ".8s" })
+    $("#nttLine").css({"width":"150px", "transition": ".8s"})
+})
+$("#nttCover").mouseleave(function () {
+    // $("#nttCoverTitle").css("color", "white")
+    $(".nttImg").css("opacity", ".5")
+    $("#nttLine").css({"width":"300px", "transition": ".8s"})
+})
+$("#fitCover").mouseenter(function () {
+    // $("#fitCoverTitle").css("color", "black");
+    $(".fitImg").css({ "opacity": "0.8", "transition": ".8s" })
+    $("#fitLine").css({"width":"150px", "transition": ".8s"})
+})
+$("#fitCover").mouseleave(function () {
+    // $("#fitCoverTitle").css("color", "white")
+    $(".fitImg").css("opacity", ".5")
+    $("#fitLine").css({"width":"300px", "transition": ".8s"})
+})
