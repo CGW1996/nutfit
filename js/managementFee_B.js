ac11b0452901d4d8724fa20efc6b02586ca6ad62
CGW1996 <59814722+CGW1996@users.noreply.github.com>
2020-07-29 00:41:38 +0800

    add managementFee,gasData pages

---
+++ js/managementFee_B.js
@@ -0,0 +1,47 @@
+//dropdown
+var dropdown = document.getElementsByClassName("dropdown-btn");
+        var i;
+
+        for (i = 0; i < dropdown.length; i++) {
+            dropdown[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var dropdownContent = this.nextElementSibling;
+                if (dropdownContent.style.display === "block") {
+                    dropdownContent.style.display = "none";
+                } else {
+                    dropdownContent.style.display = "block";
+                }
+            });
+        }
+//charts
+var myChart = echarts.init(document.getElementById('chart'));
+
+var option = {
+    xAxis: {
+        type: 'category',
+        name: '棟',
+        data: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    },
+
+    yAxis: [
+        {
+            type: 'value',
+            scale: true,
+            name: '繳交比率%',
+            max: 100,
+            min: 0,
+        },
+    ],
+
+    series: [{
+        data: [30, 20, 15, 80, 70, 62, 30, 75],
+        type: 'bar',
+        showBackground: true,
+        backgroundStyle: {
+            color: 'rgba(220, 220, 220, 0.8)'
+        }
+
+    }]
+};
+
+myChart.setOption(option);
