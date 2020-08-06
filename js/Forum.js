ae0e1b9945cb34e6eea95106d26ee56f50f3e02e
Harrison <harrison831123@gmail.com>
2020-08-06 19:05:20 +0800

    照片隱藏跟顯示

--- js/Forum.js
+++ js/Forum.js
@@ -1,21 +1,25 @@
-function readURL(input){      //上傳照片功能
+function readURL(input) {      //上傳照片功能
 
-    if(input.files && input.files[0]){
-  
-      var imageTagID = input.getAttribute("targetID");
-  
-      var reader = new FileReader();
-  
-      reader.onload = function (e) {
-  
-         var img = document.getElementById(imageTagID);
-  
-         img.setAttribute("src", e.target.result)
-  
-      }
-  
-      reader.readAsDataURL(input.files[0]);
-  
+  if (input.files && input.files[0]) {
+
+    var imageTagID = input.getAttribute("targetID");
+
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+
+      var img = document.getElementById(imageTagID);
+
+      img.setAttribute("src", e.target.result)
+      img.style.display = "block"; 
+
     }
-  
+
+    reader.readAsDataURL(input.files[0]);
+
+
+
   }
+
+}
+
